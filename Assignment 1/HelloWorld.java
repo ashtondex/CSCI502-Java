ec4938c8ac96196d95b77606f73ba3fde6dfb0a1
ashtondex <ashton.c.dsouza@gmail.com>
2018-08-29 17:11:53 -0500

    Assignment 1.
    Assignment 1 Done. Hello World.

---
+++ Assignment 1/HelloWorld.java
@@ -0,0 +1,21 @@
+/************************************************************
+ *                                                          *
+ *  CSCI 502          Assignment 1           Fall 2018      *                                             
+ *                                                          *
+ *  Programmer: Ashton Dsouza (Z1828205)                    *  
+ *                                                          *
+ *  Date Due:   08/31/2018  11.59pm                         *                          
+ *                                                          *
+ *  Purpose:    First Program in Java                       *
+ *              Displays "Hello, World!" on output          *
+ *                                                          *
+ *                                                          * 
+ ***********************************************************/ 
+
+public class HelloWorld
+{
+  public static void main(String[] args)
+  {
+    System.out.println("Hello, World!");
+  }
+}
